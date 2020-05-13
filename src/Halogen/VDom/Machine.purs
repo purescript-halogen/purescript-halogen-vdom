b385b5046643e2f948c71f5c6ce4f432f338b1d6
Serhii Khoma <srghma@gmail.com>
2020-05-13 16:56:27 +0300

    refactor: add comments, better names for arguments

--- src/Halogen/VDom/Machine.purs
+++ src/Halogen/VDom/Machine.purs
@@ -14,12 +14,31 @@ import Prelude
 import Effect.Uncurried (EffectFn1, EffectFn2, mkEffectFn1, mkEffectFn2, runEffectFn1, runEffectFn2)
 import Unsafe.Coerce (unsafeCoerce)
 
+{-
+
+type Machine is equal to:
+
+a -> Step a b
+a -> forall state . Step b state (state -> a -> Step a b) (state -> Unit)
+a -> forall state . Step b state (state -> Machine a b) (state -> Unit)
+
+where
+
+a is input
+b is output
+state is hidden state
+(state -> a -> Step a b) is a functon from state and input to the new Step
+(state -> Unit) is finalizer
+
+-}
+
 type Machine a b = EffectFn1 a (Step a b)
 
 data Step' a b s = Step b s (EffectFn2 s a (Step a b)) (EffectFn1 s Unit)
 
 foreign import data Step ∷ Type → Type → Type
 
+-- hides state type, makes it exsistential
 mkStep ∷ ∀ a b s. Step' a b s → Step a b
 mkStep = unsafeCoerce
 
---
+++ test/TestVdom.purs
@@ -0,0 +1,47 @@
+module Test.TestVdom where
+
+import Prelude
+
+import Data.Bifunctor (bimap)
+import Data.Function.Uncurried as Fn
+import Data.Maybe (Maybe(..))
+import Data.Newtype (class Newtype, un)
+import Data.Tuple (Tuple)
+import Halogen.VDom as V
+import Halogen.VDom.DOM.Prop (Prop(..), propFromString, buildProp)
+import Halogen.VDom.Thunk (Thunk, thunk1, buildThunk)
+import Unsafe.Coerce (unsafeCoerce)
+import Web.DOM.Document (Document) as DOM
+
+infixr 1 prop as :=
+
+prop ∷ ∀ a. String → String → Prop a
+prop key val = Property key (propFromString val)
+
+newtype VDom a = VDom (V.VDom (Array (Prop a)) (Thunk VDom a))
+
+instance functorHtml ∷ Functor VDom where
+  map f (VDom vdom) = VDom (bimap (map (map f)) (map f) vdom)
+
+derive instance newtypeVDom ∷ Newtype (VDom a) _
+
+elem ∷ ∀ a. String → Array (Prop a) → Array (VDom a) → VDom a
+elem n a c = VDom $ V.Elem Nothing (V.ElemName n) a (unsafeCoerce c)
+
+keyed ∷ ∀ a. String → Array (Prop a) → Array (Tuple String (VDom a)) → VDom a
+keyed n a c = VDom $ V.Keyed Nothing (V.ElemName n) a (unsafeCoerce c)
+
+text ∷ ∀ a. String → VDom a
+text a = VDom $ V.Text a
+
+thunk ∷ ∀ a b. (a → VDom b) → a → VDom b
+thunk render val = VDom $ V.Widget $ Fn.runFn2 thunk1 render val
+
+mkSpec
+  ∷ DOM.Document
+  → V.VDomSpec (Array (Prop Void)) (Thunk VDom Void)
+mkSpec document = V.VDomSpec
+  { buildWidget: buildThunk (un VDom)
+  , buildAttributes: buildProp (const (pure unit))
+  , document
+  }
