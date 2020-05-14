bbf6ec21f8aeba8251c56fed2ee5775074d0b651
Serhii Khoma <srghma@gmail.com>
2020-05-14 21:31:39 +0300

    refactor: DOM.Prop -> move functions outside of buildProp, add el and emit to PropState

--- src/Halogen/VDom/DOM/Prop.purs
+++ src/Halogen/VDom/DOM/Prop.purs
@@ -93,12 +93,14 @@ propFromInt = unsafeCoerce
 propFromNumber ∷ Number → PropValue
 propFromNumber = unsafeCoerce
 
-type EmitterInputBuilder a = DOM.Event -> Maybe a
+type EmitterInputBuilder a = DOM.Event → Maybe a
 type EventListenerAndCurrentEmitterInputBuilder a = Tuple DOM.EventListener (Ref.Ref (EmitterInputBuilder a))
 
 type PropState a =
-  { events :: Object.Object (EventListenerAndCurrentEmitterInputBuilder a)
-  , props :: Object.Object (Prop a)
+  { events ∷ Object.Object (EventListenerAndCurrentEmitterInputBuilder a)
+  , props ∷ Object.Object (Prop a)
+  , el ∷ DOM.Element
+  , emit ∷ a → Effect Unit
   }
 
 -- | A `Machine`` for applying attributes, properties, and event handlers.
@@ -115,141 +117,163 @@ buildProp emit el = renderProp
   -- on next step - patches prop
   -- on halt - all ref watchers are notified that element is removed
 
-  renderProp :: EFn.EffectFn1 (Array (Prop a)) (Step (Array (Prop a)) Unit)
+  renderProp ∷ EFn.EffectFn1 (Array (Prop a)) (Step (Array (Prop a)) Unit)
   renderProp = EFn.mkEffectFn1 \ps1 → do
-    (events :: STObject' (EventListenerAndCurrentEmitterInputBuilder a)) ← Util.newMutMap
+    (events ∷ STObject' (EventListenerAndCurrentEmitterInputBuilder a)) ← Util.newMutMap
 
     -- for each prop in array:
     --   if prop is attr - set attr to element, store attr under "attr/XXX" key in a returned object
     --   if prop is property - set property to element, store property under "prop/XXX" key in a returned object
     --   if prop is handler for DOM.EventType - start listen and add listener to `events` mutable map, store handler under "handler/EVENTTYPE" in a returned object
     --   if prop is ref updater - store `emitterInputBuilder` in under a `ref` key in a returned object, call `emitter` on creation of all props (now) and on halt of all props (later)
-    (props :: Object.Object (Prop a)) ← EFn.runEffectFn3 Util.strMapWithIxE ps1 propToStrKey (applyProp events)
+    (props ∷ Object.Object (Prop a)) ← EFn.runEffectFn3 Util.strMapWithIxE ps1 propToStrKey (Fn.runFn3 applyProp el emit events)
     let
-      (state :: PropState a) =
+      (state ∷ PropState a) =
         { events: Util.unsafeFreeze events
         , props
+        , el
+        , emit
         }
     pure $ mkStep $ Step unit state patchProp haltProp
 
-  patchProp ::
-    EFn.EffectFn2
-    (PropState a)
-    (Array (Prop a))
-    (Step (Array (Prop a)) Unit)
-  patchProp = EFn.mkEffectFn2 \state ps2 → do
-    events ← Util.newMutMap
-    let
-      { events: prevEvents, props: ps1 } = state
-      onThese = Fn.runFn2 diffProp prevEvents events
-      onThis = removeProp prevEvents
-      onThat = applyProp events
-    props ← EFn.runEffectFn6 Util.diffWithKeyAndIxE ps1 ps2 propToStrKey onThese onThis onThat
-    let
-      nextState =
-        { events: Util.unsafeFreeze events
-        , props
-        }
-    pure $ mkStep $ Step unit nextState patchProp haltProp
+patchProp ::
+  ∀ a
+  . EFn.EffectFn2
+  (PropState a)
+  (Array (Prop a))
+  (Step (Array (Prop a)) Unit)
+patchProp = EFn.mkEffectFn2 \state ps2 → do
+  events ← Util.newMutMap
+  let
+    { events: prevEvents, props: ps1, emit, el } = state
+    onThese = Fn.runFn3 diffProp el prevEvents events
+    onThis = Fn.runFn2 removeProp el prevEvents
+    onThat = Fn.runFn3 applyProp el emit events
+  props ← EFn.runEffectFn6 Util.diffWithKeyAndIxE ps1 ps2 propToStrKey onThese onThis onThat
+  let
+    nextState = -- TODO: reuse prev object
+      { events: Util.unsafeFreeze events
+      , props
+      , el
+      , emit
+      }
+  pure $ mkStep $ Step unit nextState patchProp haltProp
 
-  haltProp
-    :: EFn.EffectFn1
-    (PropState a)
-    Unit
-  haltProp = EFn.mkEffectFn1 \state → do
-    case Object.lookup "ref" state.props of
-      Just (Ref emitterInputBuilder) →
-        EFn.runEffectFn1 mbEmit (emitterInputBuilder (Removed el))
-      _ → pure unit
-
-  mbEmit :: EFn.EffectFn1 (Maybe a) Unit
-  mbEmit = EFn.mkEffectFn1 case _ of
-    Just a → emit a
+haltProp
+  ∷ ∀ a
+  . EFn.EffectFn1
+  (PropState a)
+  Unit
+haltProp = EFn.mkEffectFn1 \state → do
+  case Object.lookup "ref" state.props of
+    Just (Ref emitterInputBuilder) →
+      EFn.runEffectFn2 mbEmit state.emit (emitterInputBuilder (Removed state.el))
     _ → pure unit
 
-  applyProp
-    :: STObject' (EventListenerAndCurrentEmitterInputBuilder a)
-    -> EFn.EffectFn3 String Int (Prop a) (Prop a)
-  applyProp events = EFn.mkEffectFn3 \_ _ v →
-    case v of
-      Attribute ns attr val → do
-        EFn.runEffectFn4 Util.setAttribute (toNullable ns) attr val el
-        pure v
-      Property prop val → do
-        EFn.runEffectFn3 setProperty prop val el
-        pure v
-      Handler (DOM.EventType eventType) emitterInputBuilder → do
-        case Fn.runFn2 Util.unsafeGetAny eventType events of
-          -- if eventType is already present in events storage / listened
-          handler | Fn.runFn2 Util.unsafeHasAny eventType events → do
-            -- replace current event listener with new
-            Ref.write emitterInputBuilder (snd handler)
-            pure v
-          _ → do
-            ref ← Ref.new emitterInputBuilder
-            listener ← DOM.eventListener \ev → do
-              (emitterInputBuilder' :: EmitterInputBuilder a) ← Ref.read ref
-              EFn.runEffectFn1 mbEmit (emitterInputBuilder' ev)
+mbEmit
+  ∷ ∀ a
+  . EFn.EffectFn2
+  (a → Effect Unit)
+  (Maybe a)
+  Unit
+mbEmit = EFn.mkEffectFn2 \emit ma → case ma of
+  Just a → emit a
+  _ → pure unit
 
-            -- set/add to events map, key is eventType, value contains element listener (so we can remove it on halt) AND current emitterInputBuilder
-            EFn.runEffectFn3 Util.pokeMutMap eventType (Tuple listener ref) events
+applyProp
+  ∷ ∀ a
+  . Fn.Fn3
+  DOM.Element
+  (a → Effect Unit)
+  (STObject' (EventListenerAndCurrentEmitterInputBuilder a))
+  (EFn.EffectFn3 String Int (Prop a) (Prop a))
+applyProp = Fn.mkFn3 \el emit events → EFn.mkEffectFn3 \_ _ v →
+  case v of
+    Attribute ns attr val → do
+      EFn.runEffectFn4 Util.setAttribute (toNullable ns) attr val el
+      pure v
+    Property prop val → do
+      EFn.runEffectFn3 setProperty prop val el
+      pure v
+    Handler (DOM.EventType eventType) emitterInputBuilder → do
+      case Fn.runFn2 Util.unsafeGetAny eventType events of
+        -- if eventType is already present in events storage / listened
+        handler | Fn.runFn2 Util.unsafeHasAny eventType events → do
+          -- replace current event listener with new
+          Ref.write emitterInputBuilder (snd handler)
+          pure v
+        _ → do
+          ref ← Ref.new emitterInputBuilder
+          listener ← DOM.eventListener \ev → do
+            (emitterInputBuilder' ∷ EmitterInputBuilder a) ← Ref.read ref
+            EFn.runEffectFn2 mbEmit emit (emitterInputBuilder' ev)
 
-            -- listen events of that type on the element
-            EFn.runEffectFn3 Util.addEventListener eventType listener el
-            pure v
-      Ref emitterInputBuilder → do
-        EFn.runEffectFn1 mbEmit (emitterInputBuilder (Created el))
-        pure v
+          -- set/add to events map, key is eventType, value contains element listener (so we can remove it on halt) AND current emitterInputBuilder
+          EFn.runEffectFn3 Util.pokeMutMap eventType (Tuple listener ref) events
 
-  diffProp
-    :: Fn.Fn2
-    (Object.Object (EventListenerAndCurrentEmitterInputBuilder a))
-    (STObject' (EventListenerAndCurrentEmitterInputBuilder a))
-    (EFn.EffectFn4 String Int (Prop a) (Prop a) (Prop a))
-  diffProp = Fn.mkFn2 \prevEvents events → EFn.mkEffectFn4 \_ _ v1 v2 →
-    case v1, v2 of
-      Attribute _ _ val1, Attribute ns2 attr2 val2 →
-        if val1 == val2
-          then pure v2
-          else do
-            EFn.runEffectFn4 Util.setAttribute (toNullable ns2) attr2 val2 el
-            pure v2
-      Property _ val1, Property prop2 val2 →
-        case Fn.runFn2 Util.refEq val1 val2, prop2 of
-          true, _ →
-            pure v2
-          _, "value" → do
-            let elVal = Fn.runFn2 unsafeGetProperty "value" el
-            if Fn.runFn2 Util.refEq elVal val2
-              then pure v2
-              else do
-                EFn.runEffectFn3 setProperty prop2 val2 el
-                pure v2
-          _, _ → do
-            EFn.runEffectFn3 setProperty prop2 val2 el
-            pure v2
-      Handler _ _, Handler (DOM.EventType ty) emitterInputBuilder → do
-        let
-          handler = Fn.runFn2 Util.unsafeLookup ty prevEvents
-        Ref.write emitterInputBuilder (snd handler)
-        EFn.runEffectFn3 Util.pokeMutMap ty handler events
-        pure v2
-      _, _ →
-        pure v2
+          -- listen events of that type on the element
+          EFn.runEffectFn3 Util.addEventListener eventType listener el
+          pure v
+    Ref emitterInputBuilder → do
+      EFn.runEffectFn2 mbEmit emit (emitterInputBuilder (Created el))
+      pure v
 
-  removeProp :: Object.Object (EventListenerAndCurrentEmitterInputBuilder a) -> EFn.EffectFn2 String (Prop a) Unit
-  removeProp prevEvents = EFn.mkEffectFn2 \_ v →
-    case v of
-      Attribute ns attr _ →
-        EFn.runEffectFn3 Util.removeAttribute (toNullable ns) attr el
-      Property prop _ →
-        EFn.runEffectFn2 removeProperty prop el
-      Handler (DOM.EventType ty) _ → do
-        let
-          handler = Fn.runFn2 Util.unsafeLookup ty prevEvents
-        EFn.runEffectFn3 Util.removeEventListener ty (fst handler) el
-      Ref _ →
-        pure unit
+diffProp
+  ∷ ∀ a
+  . Fn.Fn3
+  DOM.Element
+  (Object.Object (EventListenerAndCurrentEmitterInputBuilder a))
+  (STObject' (EventListenerAndCurrentEmitterInputBuilder a))
+  (EFn.EffectFn4 String Int (Prop a) (Prop a) (Prop a))
+diffProp = Fn.mkFn3 \el prevEvents events → EFn.mkEffectFn4 \_ _ v1 v2 →
+  case v1, v2 of
+    Attribute _ _ val1, Attribute ns2 attr2 val2 →
+      if val1 == val2
+        then pure v2
+        else do
+          EFn.runEffectFn4 Util.setAttribute (toNullable ns2) attr2 val2 el
+          pure v2
+    Property _ val1, Property prop2 val2 →
+      case Fn.runFn2 Util.refEq val1 val2, prop2 of
+        true, _ →
+          pure v2
+        _, "value" → do
+          let elVal = Fn.runFn2 unsafeGetProperty "value" el
+          if Fn.runFn2 Util.refEq elVal val2
+            then pure v2
+            else do
+              EFn.runEffectFn3 setProperty prop2 val2 el
+              pure v2
+        _, _ → do
+          EFn.runEffectFn3 setProperty prop2 val2 el
+          pure v2
+    Handler _ _, Handler (DOM.EventType ty) emitterInputBuilder → do
+      let
+        handler = Fn.runFn2 Util.unsafeLookup ty prevEvents
+      Ref.write emitterInputBuilder (snd handler)
+      EFn.runEffectFn3 Util.pokeMutMap ty handler events
+      pure v2
+    _, _ →
+      pure v2
+
+removeProp
+  ∷ ∀ a
+  . Fn.Fn2
+  DOM.Element
+  (Object.Object (EventListenerAndCurrentEmitterInputBuilder a))
+  (EFn.EffectFn2 String (Prop a) Unit)
+removeProp = Fn.mkFn2 \el prevEvents → EFn.mkEffectFn2 \_ v →
+  case v of
+    Attribute ns attr _ →
+      EFn.runEffectFn3 Util.removeAttribute (toNullable ns) attr el
+    Property prop _ →
+      EFn.runEffectFn2 removeProperty prop el
+    Handler (DOM.EventType ty) _ → do
+      let
+        handler = Fn.runFn2 Util.unsafeLookup ty prevEvents
+      EFn.runEffectFn3 Util.removeEventListener ty (fst handler) el
+    Ref _ →
+      pure unit
 
 propToStrKey ∷ ∀ i. Prop i → String
 propToStrKey = case _ of
@@ -267,8 +291,8 @@ unsafeGetProperty = Util.unsafeGetAny
 
 removeProperty ∷ EFn.EffectFn2 String DOM.Element Unit
 removeProperty = EFn.mkEffectFn2 \key el →
-  EFn.runEffectFn3 Util.hasAttribute (null :: Nullable Namespace) key el >>= if _ -- If attr exists on element
-    then EFn.runEffectFn3 Util.removeAttribute (null :: Nullable Namespace) key el -- remove it using el.removeAttribute()
+  EFn.runEffectFn3 Util.hasAttribute (null ∷ Nullable Namespace) key el >>= if _ -- If attr exists on element
+    then EFn.runEffectFn3 Util.removeAttribute (null ∷ Nullable Namespace) key el -- remove it using el.removeAttribute()
     else case typeOf (Fn.runFn2 Util.unsafeGetAny key el) of
       "string" → EFn.runEffectFn3 Util.unsafeSetAny key "" el -- If it's property - set it to ""
       _        → case key of
